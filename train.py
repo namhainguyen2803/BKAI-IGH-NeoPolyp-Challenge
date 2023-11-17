79c07d49578d1b7315ec22cdbe2bcd867a1c8b82
namhainguyen2803 <hai.nn214894@sis.hust.edu.vn>
2023-11-17 20:13:11 +0700

    push

--- train.py
+++ train.py
@@ -13,11 +13,6 @@ from torch.utils.data import DataLoader
 import wandb
 import argparse
 
-# from pydrive.auth import GoogleAuth
-# from pydrive.drive import GoogleDrive
-
-from google_drive_downloader import GoogleDriveDownloader
-
 DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 
